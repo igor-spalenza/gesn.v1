7e6363b804a891da7bc3b87c01ca7ef5e6a4095c
De Morais <ecavalcanti@alfapeople.com>
2026-01-30 18:37:32 -0300

    Correções no fluxo de update de Offer

--- gesn.webApp/Infrastructure/Mappings/OfferMappingConfig.cs
+++ gesn.webApp/Infrastructure/Mappings/OfferMappingConfig.cs
@@ -16,12 +16,24 @@ namespace gesn.webApp.Infrastructure.Mappings
 
             config.NewConfig<OfferUpdateViewModel, Offer>()
                 .IgnoreNullValues(false)
-                .Ignore(dest => dest.Id)
-                .Ignore(dest => dest.CreatedAt)
-                .Ignore(dest => dest.CreatedBy)
-                //.Ignore(dest => dest.StateCode)
-                .Map(dest => dest.LastModifiedAt, src => DateTime.UtcNow)
-                .Map(dest => dest.LastModifiedBy, src => "TESTE IMPLEMENTAÇÃO ALTERAR");
+                .Map(dest => dest.CategoryId, src => src.CategoryId.ToString())
+                .Map(dest => dest.Id, src => src.Id.ToString())
+                .Map(dest => dest.SKU, src => src.SKU)
+                .Map(dest => dest.ImageUrl, src => src.ImageUrl ?? string.Empty)
+                .Map(dest => dest.CreatedAt, src => src.CreatedAt)
+                .Map(dest => dest.CreatedBy, src => src.CreatedBy)
+                .Map(dest => dest.LastModifiedAt, src => DateTime.Now)
+                .Map(dest => dest.LastModifiedBy, src => "TESTE IMPLEMENTAÇÃO ALTERAR")
+                .Map(dest => dest.AssemblyInstructions, src => src.AssemblyInstructions)
+                .Map(dest => dest.AssemblyTime, src => src.AssemblyTime)
+                .Map(dest => dest.Price, src => src.Price)
+                .Map(dest => dest.Name, src => src.Name)
+                .Map(dest => dest.Note, src => src.Note)
+                .Map(dest => dest.Cost, src => src.Cost)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.QuantityPrice, src => src.QuantityPrice)
+                .Map(dest => dest.UnitPrice, src => src.UnitPrice)
+                ;
 
             config.NewConfig<Offer, OfferSummaryViewModel>()
                 .Map(dest => dest.CategoryName, src => src.CategoryNavigation.Name);
